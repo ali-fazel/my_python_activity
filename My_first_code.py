11716ded3e48c2fc940cea8ac4b20f2260a511de
ali-fazel <108989982+ali-fazel@users.noreply.github.com>
2022-09-14 18:23:12 +0100

    Practical activity - analysing real estate

---
+++ My_first_code.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code
+
+print ('My name is Ali Fazel')
+print ('The date is 14 September 2022')
+
+
+# In[4]:
+
+
+# Write a program to analyse real estate data
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Total sales for December 
+x = a + b + c + d + e
+
+print (x)
+
+# Average sale price for December
+y = 182900 / 5
+
+print (y)
+
+# Property price for property ID C 
+z = c - 6500
+
+print (z)
+
